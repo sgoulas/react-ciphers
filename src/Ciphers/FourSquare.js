9350170ba3618cabb1cbf526e9dfc4c220d28aff
sgoulas <sprgoulas@gmail.com>
2020-06-07 17:19:08 +0300

    adds four square decryption

--- src/Ciphers/FourSquare.js
+++ src/Ciphers/FourSquare.js
@@ -29,7 +29,7 @@ export const Encrypt = (input, key_1, key_2) => {
   return ciphertext.toUpperCase();
 };
 
-export const GenRandKey = (k) => {
+export const GenRandKey = () => {
   const keychars = "abcdefghiklmnopqrstuvwxyz";
   const chars = keychars.split("");
   let key = "";
@@ -41,6 +41,39 @@ export const GenRandKey = (k) => {
   }
 
   return key.toUpperCase();
+};
+
+export const Decrypt = (encryptedText, key_1, key_2) => {
+  let ciphertext = encryptedText
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, "")
+    .replace(/[j]/g, "i");
+  let k1 = key_1.toLowerCase().replace(/[^a-z]/g, "");
+  let k2 = key_2.toLowerCase().replace(/[^a-z]/g, "");
+  // do some error checking
+  if (ciphertext.length < 1) {
+    alert("please enter some plaintext (letters and numbers only)");
+    return;
+  }
+  if (k1.length !== 25 || k2.length !== 25) {
+    alert("keysquare must be 25 characters in length");
+    return;
+  }
+  if (ciphertext.length % 2 === 1) {
+    alert("ciphertext should be even length (wrong algorithm?)");
+    return;
+  }
+  let plaintext = "";
+  let rt = "abcdefghiklmnopqrstuvwxyz";
+  for (let i = 0; i < ciphertext.length; i += 2) {
+    let a = k1.indexOf(ciphertext.charAt(i)) % 5;
+    let b = parseInt(k1.indexOf(ciphertext.charAt(i)) / 5);
+    let c = k2.indexOf(ciphertext.charAt(i + 1)) % 5;
+    let d = parseInt(k2.indexOf(ciphertext.charAt(i + 1)) / 5);
+    plaintext += rt.charAt(5 * b + c);
+    plaintext += rt.charAt(5 * d + a);
+  }
+  return plaintext.toUpperCase();
 };
 
 // http://practicalcryptography.com/ciphers/classical-era/four-square/
