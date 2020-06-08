15410191e022c296fa754cd1a55e6c993fba5f40
sgoulas <sprgoulas@gmail.com>
2020-06-08 16:16:17 +0300

    :construction: adds input placeholder

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import React from "react";
 import Page from "./components/Page/Page";
 import CipherCards from "./components/CipherCards/CipherCards";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
 
 const App = () => {
   return (
     <>
       <CssBaseline />
       <Page>
-        <CipherCards />
+        <Grid
+          container
+          direction="column"
+          justify="center"
+          alignItems="center"
+          spacing={3}
+        >
+          <Grid item xs={12}>
+            <span>here be the input jumbo</span>
+          </Grid>
+          <Grid item xs={12}>
+            <CipherCards />
+          </Grid>
+        </Grid>
       </Page>
     </>
   );
