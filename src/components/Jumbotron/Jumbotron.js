387c96a36fa2acf3ef9f9cdacc62800634287205
sgoulas <sprgoulas@gmail.com>
2020-06-13 23:19:08 +0300

    styles jumbotron

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
   root: {
@@ -8,12 +9,24 @@ const useStyles = makeStyles({
     backgroundColor: "#F4A261",
     padding: 0,
   },
+  title: {
+    fontSize: 68,
+  },
+  subTitle: {
+    fontSize: 22,
+  },
 });
 
 const Jumbotron = () => {
   const classes = useStyles();
-  const title = "React Ciphers";
-  const subTitle = "A collection of dynamic examples";
+  const title = (
+    <Typography className={classes.title}> React Ciphers</Typography>
+  );
+  const subTitle = (
+    <Typography className={classes.subTitle}>
+      A collection of dynamic examples
+    </Typography>
+  );
 
   return (
     <Grid
